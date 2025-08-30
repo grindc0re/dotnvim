7a89bafc4c4df868f6dcfd782cb1e53a274ed946
Joakim Vinteräng <joakim@greenfire.se>
2025-08-30 09:15:57 +0200

    Add diagnostic icons to signcolumn.

--- init.lua
+++ init.lua
@@ -70,6 +70,24 @@ vim.o.termguicolors = true
 vim.cmd('syntax enable')
 vim.cmd('colorscheme onedark')
 
+-- Diagnostic errors in signcolumn
+vim.diagnostic.config({
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = ' ',
+      [vim.diagnostic.severity.WARN] = ' ',
+      [vim.diagnostic.severity.INFO] = '  ',
+      [vim.diagnostic.severity.HINT] = ' ',
+    },
+    linehl = {
+      [vim.diagnostic.severity.ERROR] = 'ErrorMsg',
+    },
+    numhl = {
+      [vim.diagnostic.severity.WARN] = 'WarningMsg',
+    },
+  }
+})
+
 -- ---------------------------------------------------------------------------------
 -- Keymaps
 -- ---------------------------------------------------------------------------------
