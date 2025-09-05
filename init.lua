1410b9931d6e9de94cc2ce56084e280ad0e93044
Joakim Vinteräng <joakim.vinterang@unikom.se>
2025-09-05 21:40:37 +0200

    Format on save.

--- init.lua
+++ init.lua
@@ -277,5 +277,19 @@ vim.lsp.enable({
   'html',
   'htmx',
   'jsonls',
+  'ruff',
   'tailwindcss',
+})
+
+-- Autoformat on save
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("lsp", { clear = true }),
+  callback = function(args)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = args.buf,
+      callback = function()
+        vim.lsp.buf.format({async = false, id = args.data.client_id })
+      end,
+    })
+  end
 })
