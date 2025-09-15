c3220bc5cf2d6c75c6254952810ffd414f0300b2
Joakim Vinteräng <joakim.vinterang@unikom.se>
2025-09-15 20:21:51 +0200

    Use filetype based indent.

--- init.lua
+++ init.lua
@@ -56,6 +56,8 @@ vim.o.tabstop = 2
 vim.o.softtabstop = 2
 vim.o.shiftwidth = 2
 vim.o.expandtab = true
+vim.o.autoindent = false
+vim.o.smartindent = false
 vim.cmd('filetype plugin indent on')
 
 -- Set updatetime to speed up gitgutter hints.
