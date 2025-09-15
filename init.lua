be1f8838982ba0dcacc9d3c59351d396ed3e9896
Joakim Vinteräng <joakim.vinterang@unikom.se>
2025-09-15 20:18:29 +0200

    Use K for lsp hover.

--- init.lua
+++ init.lua
@@ -136,7 +136,7 @@ vim.g["conjure#mapping#doc_word"] = false
 vim.g["conjure#mapping#doc_word"] = "gk"
 
 -- Reset it to the default unprefixed K (note the special table wrapped syntax)
-vim.g["conjure#mapping#doc_word"] = {"K"}
+-- vim.g["conjure#mapping#doc_word"] = {"K"}
 
 -- ---------------------------------------------------------------------------------
 -- Autocommands
@@ -314,4 +314,19 @@ require("conform").setup({
       stdin = false,
     },
   },
+})
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup('lsp_attach_disable_ruff_hover', { clear = true }),
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client == nil then
+      return
+    end
+    if client.name == 'ruff' then
+      -- Disable hover in favor of Pyright
+      client.server_capabilities.hoverProvider = false
+    end
+  end,
+  desc = 'LSP: Disable hover capability from Ruff',
 })
